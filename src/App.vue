4b8d3114f031ab046b33af35464965013a521108
Nikita Krupin <krupin.nikita0110@gmail.com>
2022-02-19 21:23:49 -0500

    logo animation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,45 @@
 <template>
+  <div class="bg-neutral-100 rounded-b-3xl py-24 grid justify-center">
+    <div
+      class="neumorphic-shadow bg-neutral-100 rounded-3xl grid content-center h-32 w-32"
+    >
+      <img class="logo" src="rwc.png" alt="logo" />
+    </div>
+  </div>
   <nav>
     <router-link to="/">Home</router-link>
   </nav>
   <router-view />
 </template>
 
-<style></style>
+<style>
+.neumorphic-shadow {
+  box-shadow: #ddd 1rem 1rem 1rem 0 inset, #fff -1rem -1rem 1rem 0 inset;
+}
+.logo {
+  filter: drop-shadow(0 0.5rem 1rem #3a0e66);
+  transition-duration: 0.3s;
+  transition-property: opacity, transform;
+  transform: scale(0);
+  opacity: 0;
+  animation: popin 0.5s 0.3s ease-out 1 forwards;
+}
+@keyframes popin {
+  0% {
+    opacity: 0;
+    transform: scale(0);
+  }
+  33% {
+    opacity: 1;
+    transform: scale(1.5);
+  }
+  66% {
+    opacity: 1;
+    transform: scale(0.75);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+</style>
