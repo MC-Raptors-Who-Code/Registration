d43f0e430a900cbf904311a9a056a982618cdf4a
Nikita Krupin <krupin.nikita0110@gmail.com>
2022-03-23 19:04:14 -0400

    404 not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Form from "../views/Form.vue";
 import Grats from "../views/Grats.vue";
+import NotFound from "../views/404.vue";
 
 const routes = [
   {
@@ -13,11 +14,15 @@ const routes = [
     name: "grats",
     component: Grats,
   },
+  {
+    path: '/:pathMatch(.*)',
+    component: NotFound
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/:pathMatch(.*)', component: Form }],
+  routes,
 });
 
 export default router;
---
+++ src/views/404.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <img class="mx-auto" style="max-height: 30vh;" src="404.webp" />
+  </div>
+</template>
