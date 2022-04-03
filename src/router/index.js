f3d10925841eafc7f818a686097c30affe2d3bd4
Nikita Krupin <krupin.nikita0110@gmail.com>
2022-04-02 20:45:28 -0400

    WebHashHistory should fix gh routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Form from "../views/Form.vue";
 import Grats from "../views/Grats.vue";
 import NotFound from "../views/404.vue";
@@ -21,7 +21,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
